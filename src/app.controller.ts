e0a9f5ad91dfd1fd71ba58e5ed36fb48d6ee20e4
ogneyar <ogneyar-ne@ya.ru>
2021-01-17 16:03:53 +0300

    add more methods

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post, Put, Head, Options } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -12,6 +12,32 @@ export class AppController {
 
   @Post()
   postHello(): string {
-    return this.appService.getHello();
+    return this.appService.postHello();
   }
+
+  @Put()
+  putHello(): string {
+    return this.appService.putHello();
+  }
+  
+  @Patch()
+  patchHello(): string {
+    return this.appService.patchHello();
+  }
+
+  @Delete()
+  deleteHello(): string {
+    return this.appService.deleteHello();
+  }
+  
+  @Head()
+  headHello(): string {
+    return this.appService.headHello(); // not work...
+  }
+
+  @Options()
+  optionsHello(): string {
+    return this.appService.optionsHello();
+  }
+
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,33 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+
   getHello(): string {
-    return 'Hello World!';
+    return 'Get: Hello Nest JS!';
   }
+
+  postHello(): string {
+    return 'Post: Hello Nest JS!';
+  }
+
+  putHello(): string {
+    return 'Put: Hello Nest JS!';
+  }
+
+  patchHello(): string {
+    return 'Patch: Hello Nest JS!';
+  }
+
+  deleteHello(): string {
+    return 'Delete: Hello Nest JS!';
+  }
+
+  headHello(): string {
+    return 'Head: Hello Nest JS!';
+  }
+
+  optionsHello(): string {
+    return 'Options: Hello Nest JS!';
+  }
+
 }
